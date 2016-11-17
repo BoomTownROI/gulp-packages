026c6c5e6e122d584478520ec171bb64efbc865f
JJ Jacobs <jrjacobs24@gmail.com>
2016-11-17 15:45:31 -0500

    CNS-3266: Added custom helper to our PHP that matches a custom helper in hbs
    - added the ‘or’ custom helper to our hbs->php compiler
    - Cleaned up some of our formatting a bit. Seems like it got all wonky, perhaps in a copy/paste

--- index.js
+++ index.js
@@ -10,47 +10,45 @@ var gutil = require('gulp-util'),
 
 function lightncandy(opts) {
 
-	opts = _.extend({
-
-    }, opts || {});
+	opts = _.extend({}, opts || {});
 
 	return through.obj(function(file, enc, callback){
 
 		// Pass file through if:
-        // - file has no contents
-        // - file is a directory
-        if ( file.isNull() || file.isDirectory() ) {
-            this.push(file);
-            return callback();
-        }
+    // - file has no contents
+    // - file is a directory
+    if ( file.isNull() || file.isDirectory() ) {
+        this.push(file);
+        return callback();
+    }
 
-        if (['.hbs', '.handlebars', '.tmpl', '.mustache'].indexOf(path.extname(file.path)) === -1) {
-            this.emit('error', new PluginError({
-                plugin: 'LightnCandy',
-                message: 'Supported formats include Handlebars and Mustache templates only.'
-            }));
-            return callback();
-        }
+    if (['.hbs', '.handlebars', '.tmpl', '.mustache'].indexOf(path.extname(file.path)) === -1) {
+        this.emit('error', new PluginError({
+            plugin: 'LightnCandy',
+            message: 'Supported formats include Handlebars and Mustache templates only.'
+        }));
+        return callback();
+    }
 
-        // No support for streams
-        if (file.isStream()) {
-            this.emit('error', new PluginError({
-                plugin: 'LightnCandy',
-                message: 'Streams are not supported.'
-            }));
-            return callback();
-        }
+    // No support for streams
+    if (file.isStream()) {
+        this.emit('error', new PluginError({
+            plugin: 'LightnCandy',
+            message: 'Streams are not supported.'
+        }));
+        return callback();
+    }
 
-        //./process --template templates/listings/hero/single.hbs --dest template/listings/hero/single.php
-        // More compile options https://github.com/zordius/lightncandy
-        var template = file.path.substr(file.path.indexOf("templates"));
-        var dest = template.replace(path.extname(template), '.php');
+    //./process --template templates/listings/hero/single.hbs --dest template/listings/hero/single.php
+    // More compile options https://github.com/zordius/lightncandy
+    var template = file.path.substr(file.path.indexOf("templates"));
+    var dest = template.replace(path.extname(template), '.php');
 
-        command = 'node_modules/gulp-lightncandy/process ';
-        command += '--template ' + template + ' ';
-        command += '--dest ' + dest;
+    command = 'node_modules/gulp-lightncandy/process ';
+    command += '--template ' + template + ' ';
+    command += '--dest ' + dest;
 
-        exec(command, function (error, stdout, stderr) {
+    exec(command, function (error, stdout, stderr) {
 			if ( stderr ) {
 				gutil.log(stderr);
 			}
@@ -63,14 +61,10 @@ function lightncandy(opts) {
 
 			// call user callback if ano error occurs
 			if ( error ) {
-
-				gutil.log(error);
-        		return callback(error, file);
-
+				  gutil.log(error);
+        	return callback(error, file);
 			} else {
-
-        		return callback(null, file);
-
+        	return callback(null, file);
 			}
 
 		});
